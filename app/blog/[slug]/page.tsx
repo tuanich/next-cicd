dd59defaa11d6ddd2cda00bd3cd5b476606cc90b
tuanich <53761195+tuanich@users.noreply.github.com>
2024-10-11 20:49:50 +0700

    update10

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,8 +8,6 @@ export default function Page({ params }: Params) {
   return <>
     <h1>Slug: {params.slug}</h1>
     <p>HoleTex</p>
-    <p>Hole Tex to HCM</p>
-    <p>Hole Tex to VN</p>
-    <p>Hole Tex to HN</p>
+
   </>
 }
